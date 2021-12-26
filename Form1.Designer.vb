3cbb19659458d2b70c9d086b873443c68ff0738e
noradlb1 <zcyfkhuru.bz@mail.ru>
2021-12-26 22:25:22 +0100

    Abstract-Connected-Circles-by-monstermc
    Abstract-Connected-Circles-by-monstermc

---
+++ Form1.Designer.vb
@@ -0,0 +1,49 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = true
+        Me.Timer1.Interval = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(45,Byte),Integer), CType(CType(45,Byte),Integer), CType(CType(48,Byte),Integer))
+        Me.BackgroundImage = Global.Abstract_Connected_Circles_by_monstermc.My.Resources.Resources.skull_green1
+        Me.ClientSize = New System.Drawing.Size(265, 275)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "Form1"
+        Me.Text = "H4CK3D BY MONSTERMC"
+        Me.ResumeLayout(false)
+
+End Sub
+
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ Form1.vb
@@ -0,0 +1,149 @@
+﻿Public Class Form1
+    Private MainRect As Rectangle
+    Private Shared rd As New Random
+    Private Shared Circles(250) As cCircle
+    Private Shared PointDistance As Decimal = 65
+    Private Shared ea As MouseEventArgs
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Invalidate()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        DoubleBuffered = True
+        PrepareCircles()
+    End Sub
+
+    Private Sub PrepareCircles()
+        'get form rectangle
+        MainRect = DisplayRectangle
+
+        'prepare circles
+        For i As Integer = 0 To Circles.Count - 1
+            Circles(i) = New cCircle(MainRect)
+        Next
+    End Sub
+
+    Private Sub Form1_MouseLeave(sender As Object, e As EventArgs) Handles Me.MouseLeave
+        ea = Nothing
+    End Sub
+
+    Private Sub Form1_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove
+        ea = e
+    End Sub
+
+    Private Sub Form1_Paint(sender As Object, e As PaintEventArgs) Handles Me.Paint
+        Dim G As Graphics = e.Graphics
+
+        G.SmoothingMode = Drawing2D.SmoothingMode.AntiAlias
+
+        For i As Integer = 0 To Circles.Count - 1
+
+            If Not IsNothing(ea) Then
+                Dim msx As Integer = ea.X
+                Dim msy As Integer = ea.Y
+                Dim osx As Integer = Circles(i).x
+                Dim osy As Integer = Circles(i).y
+
+                'detect the mouse location. if a circle is within the range (100px) of mouse pointer
+                'then push back the circle
+                If (msx - osx) ^ 2 + (msy - osy) ^ 2 < 100 ^ 2 Then
+                    Dim pTarget As Point = New Point(osx, osy)
+                    Dim pOrigin As Point = New Point(msx, msy)
+
+                    'get the angle of cricle from mouse pointer location
+                    Dim getAngle As Integer
+                    getAngle = (((Math.Atan2(osx - msx, msy - osy) * (180 / Math.PI)) + 360.0) Mod 360.0)
+
+                    'get the distance of circle from mouse pointer location
+                    Dim getDist As Integer = DistanceBetween(New Point(msx, msy), New Point(osx, osy))
+
+                    'get the new point where the circle should be pushed back 
+                    Dim newPoint As Point = New Point(GetX(osx, 100 - getDist, getAngle), GetY(osy, 100 - getDist, getAngle))
+
+                    'set the new point to the circle
+                    Circles(i).x = newPoint.X
+                    Circles(i).y = newPoint.Y
+                End If
+            End If
+
+            'update circles
+            Circles(i).Show(G)
+            Circles(i).Update()
+        Next
+    End Sub
+
+    Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        PrepareCircles()
+    End Sub
+
+    Friend Class cCircle
+        Public movementAngle As Decimal
+        Public speed As Decimal
+        Public size As Decimal
+        Public x As Decimal
+        Public y As Decimal
+        Private MainRect As Rectangle
+
+        Sub New(MainRect As Rectangle)
+            Me.MainRect = MainRect
+            ResetVars()
+        End Sub
+
+        Private Sub ResetVars()
+            'reset variables
+            movementAngle = rd.Next(0, 360)
+            speed = rd.Next(2, 7)
+            size = rd.Next(2, 10)
+            x = rd.Next(0, MainRect.Width)
+            y = rd.Next(0, MainRect.Height)
+        End Sub
+
+        Public Sub Show(G As Graphics)
+            Dim mypoint As Point = New Point(x, y)
+
+            'loop to all circles to identify nearby circles 
+            For i As Integer = 0 To Circles.Count - 1
+                Dim cpoint As Point = New Point(Circles(i).x, Circles(i).y)
+
+                If Circles(i).x <> x And Circles(i).y <> y Then
+                    'get the distance between 2 circles
+                    Dim iDis As Integer = DistanceBetween(mypoint, cpoint)
+                    If iDis < PointDistance Then
+                        'set the alpha of the line based on the distance
+                        'fade when far and more visible when near
+                        Dim a As Integer = (iDis / PointDistance) * 50
+                        G.DrawLine(New Pen(Color.FromArgb(50 - a, 0, 255, 0), 0.5), mypoint, cpoint)
+                    End If
+                End If
+            Next
+
+            G.FillEllipse(New SolidBrush(Color.FromArgb(255, 0, 220, 0)), New Rectangle(x - (size / 2), y - (size / 2), size, size))
+        End Sub
+
+        Public Sub Update()
+            'move the position of the circle based on the given speed and angle
+            x = GetX(x, speed, movementAngle)
+            y = GetY(y, speed, movementAngle)
+
+            'reset variables when the circle reaches the edge
+            If x < -20 Or y < -20 Or x > MainRect.Width + 20 Or y > MainRect.Height + 20 Then
+                ResetVars()
+            End If
+        End Sub
+
+    End Class
+
+    Public Shared Function DistanceBetween(p1 As Point, p2 As Point) As Single
+        Return Math.Sqrt((Math.Abs(p2.X - p1.X) ^ 2) + (Math.Abs(p2.Y - p1.Y) ^ 2))
+    End Function
+
+    Private Shared Function GetX(FromX As Decimal, toAdd As Decimal, Angle As Integer) As Decimal
+        Return FromX + toAdd * Math.Cos(If(Angle - 90 < 0, 360 + (Angle - 90), Angle - 90) * Math.PI / 180)
+    End Function
+
+    Private Shared Function GetY(FromY As Decimal, toAdd As Decimal, Angle As Integer) As Decimal
+        Return FromY + toAdd * Math.Sin(If(Angle - 90 < 0, 360 + (Angle - 90), Angle - 90) * Math.PI / 180)
+    End Function
+
+End Class
